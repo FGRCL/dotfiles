8e3ea6e184f789c5038ad4c6967591158c34ae2d
Francois LaBerge <FGCRLaBerge@gmail.com>
2026-03-28 14:42:14 -0400

    add home manager config

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+{
+	home.stateVersion = "25.11";
+	home.username = "francois";
+	home.homeDirectory = "/home/francois";
+	programs.home-manager.enable = true;
+
+	home.packages = with pkgs; [
+		fortune
+		htop
+
+		#development packages
+		firefox
+		yadm
+		nushell
+		tmux
+		lazygit
+		lazydocker
+		k9s
+
+		# neovim packages
+		neovim
+		tree-sitter
+		go
+		luarocks
+		lua
+		ruby
+		php
+		php85Packages.composer
+		nodejs_24
+		jdk25_headless
+		julia
+		cmake-language-server
+		nginx-language-server
+		python313
+		wget
+	];
+}
